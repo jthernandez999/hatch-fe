49a242b2ec12e33bbfe1d7cc381a0e673970d73f
Joe Hernandez <j.t.hernandez999@gmail.com>
2022-10-13 23:07:06 -0700

    completed assessment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Hero from "./components/Hero";
+import sunriseAlarm from "./assets/sunrise-alarm.svg";
+import sunshineHeartIcon from "./assets/sunshine-heart-icon.svg";
+import wavyLines from "./assets/wavy-lines-icon.svg";
+
+const cards = [
+    {
+        id: 1,
+        text: "Text",
+        imageUrl: wavyLines,
+    },
+    {
+        id: 2,
+        text: "Text",
+        imageUrl: sunshineHeartIcon,
+    },
+    {
+        id: 3,
+        text: "Text",
+        imageUrl: sunriseAlarm,
+    },
+];
+
+const CardListItem = (props) => {
+    return (
+        <div className='main-container'>
+            <li>
+                <div className='card-container'>
+                    <img className='cardImg' src={props.card.imageUrl} />
+                    <p>{props.card.text}</p>
+                </div>
+            </li>
+        </div>
+    );
+};
+
+const CardList = () => {
+    return (
+        <div className='card-list-container'>
+            <h1 className='card-list-section-header'> Your heading here</h1>
+            <ul className='card-list'>
+                {cards.map((card) => {
+                    return <CardListItem card={card} key={card.id} />;
+                })}
+            </ul>
+        </div>
+    );
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Hero />
+            <CardList />
+        </div>
+    );
 }
 
 export default App;
